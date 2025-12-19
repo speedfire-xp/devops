69c880ee66973cc3352e0430c6d418a1a5e4546d
speedfire-xp <k.b.acper05@gmail.com>
2025-12-19 06:00:01 +0100

    first commit

---
+++ SampleWebApiAspNetCore/Migrations/20251114181856_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SampleWebApiAspNetCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FoodItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<string>(type: "text", nullable: true),
+                    Calories = table.Column<int>(type: "integer", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FoodItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FoodItems");
+        }
+    }
+}
---
+++ SampleWebApiAspNetCore/Services/SeedDataService.cs
@@ -0,0 +1,29 @@
+﻿using SampleWebApiAspNetCore.Repositories;
+using SampleWebApiAspNetCore.Entities;
+
+
+namespace SampleWebApiAspNetCore.Services
+{
+    public class SeedDataService : ISeedDataService
+    {
+        public void Initialize(FoodDbContext context)
+        {
+            if (context.FoodItems.Any())
+            {
+                return;
+            }
+
+            var foods = new FoodEntity[]
+            {
+                new FoodEntity { Name = "Banana", Type = "Fruit", Calories = 105, Created = DateTime.UtcNow },
+                new FoodEntity { Name = "Apple", Type = "Fruit", Calories = 95, Created = DateTime.UtcNow },
+                new FoodEntity { Name = "Broccoli", Type = "Vegetable", Calories = 50, Created = DateTime.UtcNow },
+                new FoodEntity { Name = "Chicken Breast", Type = "Protein", Calories = 165, Created = DateTime.UtcNow }
+            };
+
+            context.FoodItems.AddRange(foods);
+            
+            context.SaveChanges(); 
+        }
+    }
+}
